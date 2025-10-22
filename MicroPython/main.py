1b898e52179d8d1080d2a9fbc36d520bdd0ab6a6
Xander-Henderson <xander.henderson@stu.ocsb.ca>
2025-10-22 18:28:31 +0000

    wierd code is structurally unreachable thing

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,28 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Alexander
+Created on: Oct 2025
+This module shows two random number from 0 to 99.
 """
 
 from microbit import *
+import random
 
+# setup
+random_number_one = 0
+random_number_two = 0
+display.clear()
+display.show(Image.HAPPY)
 
-display.scroll("Hello, World!")
+# gives the variable the random number
+random_number_one = random (0, 99)
+random_number_two = random (0, 99)
+
+# show number one
+while True:
+    if button_a.is_pressed():
+        display.show(random_number_one)
+
+# show number two
+while True:
+    if button_b.is_pressed():
+        display.show(random_number_two)
